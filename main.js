0ad130dbd294ec6a0563fd186dd3539abc9901e9
Shawn <sbenn9210@gmail.com>
2019-01-03 16:33:20 -0600

    Adding forms and custom form validation

--- main.js
+++ main.js
@@ -1,3 +1,69 @@
+Vue.component("product-review", {
+  template: `
+
+    <p v-if="errors.length">
+      <b>Please correct the following error(s):</b>
+      <ul>
+        <li v-for="error in errors">{{error}}
+        </li>
+      </ul>
+    </p>
+    <form class="review-form" @submit.prevent="onSubmit">
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+      </p>
+      <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit">
+      </p>
+    </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        };
+
+        this.$emit("review-submitted", productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) this.errors.push("Name required.");
+        if (!this.review) this.errors.push("Review required.");
+        if (!this.rating) this.errors.push("Rating required.");
+      }
+    }
+  }
+});
+
 Vue.component("product", {
   props: {
     premium: {
@@ -44,6 +110,18 @@ Vue.component("product", {
     <p v-else>Out of Stock</p>
   </div> -->
 </div>
+  <div>
+    <h2>Reviews</h2>
+    <p v-if="!reviews.length">There are no reviews yets.</p>
+    <ul>
+      <li v-for="review in reviews">
+        <p>{{ review.name }}</p>
+        <p>Rating: {{ review.rating }}</p>
+        <p>{{ review.review }}</p>
+      </li>
+    </ul>
+  </div>
+  <product-review @review-submitted="addReview"></product-review>
     </div>
   `,
   data() {
@@ -71,7 +149,8 @@ Vue.component("product", {
           variantQuantity: 20
         }
       ],
-      sizes: ["small", "medium", "large", "extra large"]
+      sizes: ["small", "medium", "large", "extra large"],
+      reviews: []
     };
   },
   methods: {
@@ -87,6 +166,9 @@ Vue.component("product", {
         "remove-from-cart",
         this.variants[this.selectedVariant].variantId
       );
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     }
   },
   computed: {
